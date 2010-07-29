0d7a3ec9e91853fa1a6ed76053c8b5bb131ab1b7
Jesse Vincent <jesse@bestpractical.com>
2010-07-29 12:06:09 -0400

    fixes for ticket replied to actually working.

--- lib/RT/Extension/NotificationMatrix/Rule/TicketRepliedTo.pm
+++ lib/RT/Extension/NotificationMatrix/Rule/TicketRepliedTo.pm
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 use base 'RT::Extension::NotificationMatrix::Rule';
 
-use constant NM_Entry => 'On correspondence';
+use constant NM_Entry => 'Ticket replied to';
 use constant DefaultTemplate => 'Admin Correspondence';
 use constant DefaultExternalTemplate => 'Correspondence';
-use constant Description => 'On correspondence';
+use constant Description => 'Ticket replied to';
 
 sub ConditionMatched {
     my $self = shift;
